ecb62ef35fd499478446ad9ed23489b1dc81c5e7
SeRyuu25 <18220025@mahasiswa.itb.ac.id>
2022-09-28 00:33:57 +0700

    FastAPI creation

---
+++ data.py
@@ -0,0 +1,18 @@
+from http.client import HTTPException
+import json
+from fastapi import FastAPI
+with open("data.json", "r") as read_file:
+	data = json.load(read_file)
+app = FastAPI()
+@app.get('data/{item_id}')
+async def read_data():
+	i = 0
+	dictMahasiswa = []
+	for dataMahasiswa in data['data']:
+		dictMahasiswa[i] = dataMahasiswa
+		i += 1
+	return dictMahasiswa
+
+	raise HTTPException(
+		status_code=404, detail=f'Item not found'
+	)
